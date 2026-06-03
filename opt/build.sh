7a62262bb50a8c9e9fd60ef09ffd652f2c5b6360
dr-bartanwala <jogosatoru147@gmail.com>
2026-06-03 20:32:49 +0530

    initial_commit

---
+++ opt/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+
+build_image() {
+    
+    command="${1}"
+
+    config_name="pi0w"
+    config_dir="./${config_name}"
+    rootfs_overlay="./rootfs-overlay"
+    config_file="${config_dir}/configs/pi0w"
+    build_dir="../output"
+    build2_dir="${cur_dir}/../../output"
+    image_dir="${cur_dir}/../images"
+
+    #command=menuconfig #savedefconfig #${config_name}_defconfig
+
+    echo "hello world"
+
+    echo ${command}
+
+    make BR2_EXTERNAL="../${config_dir}/" O="${build_dir}" -C ./buildroot/ ${command}
+
+}
+
+command=$1
+build_image ${command}
---
+++ opt/pi0w/external.mk
@@ -0,0 +1,2 @@
+# no use right now
+include $(sort $(wildcard $(BR2_EXTERNAL_RPI0W_Vinylla_PATH)/../external-packages/*/*.mk))
