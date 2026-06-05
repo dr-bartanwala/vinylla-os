af6a45a60d63d683f78868bc37bf273675b0807c
dr-bartanwala <jogosatoru147@gmail.com>
2026-06-06 04:18:50 +0530

    barely working wifi

--- opt/pi0w/external.mk
+++ opt/pi0w/external.mk
@@ -1,2 +1,2 @@
-# no use right now
-include $(sort $(wildcard $(BR2_EXTERNAL_RPI0W_Vinylla_PATH)/../external-packages/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_RPI0W_Vinylla_PATH)/../vinylla-applications/*/*.mk))
+
--- opt/rootfs-overlay/start.sh
+++ opt/rootfs-overlay/start.sh
@@ -1,6 +1,31 @@
+# connect to network
 #!/bin/sh
+# Buildroot typically uses standard sh, not bash
+SSID="Dr.Bhanushali"
+PASSWORD="KHUDHR@2613"
+INTERFACE="wlan0"
 
-cd /opt/src/
-./blink
+echo "Bringing up interface $INTERFACE..."
+ip link set $INTERFACE up
+
+echo "Generating wpa_supplicant configuration..."
+# Create a temporary config file for wpa_supplicant
+wpa_passphrase "$SSID" "$PASSWORD" > /tmp/wpa_supplicant.conf
+
+echo "Starting wpa_supplicant..."
+# Run wpa_supplicant in the background (-B)
+# -i specifies interface, -c specifies config file
+wpa_supplicant -B -i "$INTERFACE" -c /tmp/wpa_supplicant.conf
+
+echo "Waiting for association..."
+sleep 5
+
+echo "Requesting IP address via DHCP..."
+# udhcpc is the default lightweight DHCP client in Buildroot (BusyBox)
+udhcpc -i "$INTERFACE" -n
+
+echo "Network status:"
+ip addr show $INTERFACE
+
 
 
